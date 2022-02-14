546cb99e2b9ab035d32dbe8524294944d861a627
Joãopegnolate <joaopegnolate@gmail.com>
2022-02-14 16:56:14 -0300

    aula-variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,23 @@
+const nome= prompt ("qual o seu nome?")
+let idade= prompt ("qual a sua idade?")
+typeof 
+/*apartir de typeof tudo esta salvo??*/
+ 
+console.log(valor1, valor2) (ola nome , voce tem idade anos)
+
+
+
+
+
+
+
+let a = 10
+leb b = 25
+let c = 0
+ a = c 
+ b = a
+ c = b
+
+ console.log(a)
+ console.log(b)
+ console.log(b)
