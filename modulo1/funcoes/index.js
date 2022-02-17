34504ec6d0c574a607a46afb80a1481ed9c13e85
Joãopegnolate <joaopegnolate@gmail.com>
2022-02-17 16:53:47 -0300

    funcoes

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,33 @@
+// ex de interpretação de codigo//
+// exercicio 1-a)
+// minhaFuncao= 10
+// minhafuncao= 50 
+// os valores 10 e 50 apareceriam no console//
+
+//b)o progama daria erro pois sem o console.log o valor nao seria impresso no console.//
+
+//Exercicio 2 
+// a) transforma todos os caracteres da palavra em prompt em letras minusculas
+// e tambem vai determinar se a uma string dentro de outra//
+
+/*b) I-eu gosto de  
+ii-cenoura e bom pra vista
+iii- cenouras crescem na terra */
+//------------------------------------------------------------------------------------//
+
+//exercicio de escrita de codigo//
+//ex1//
+
+function exercicio1 () {
+ string const  =nome ("joao Augusto")
+ number const =idade ("19")
+ string1 const =cidade ("Poços de Caldas")
+ string2 const =profissão ("sou estudante") 
+
+ frase = 'eu sou' (nome) , 
+"Eu sou" [nome],"tenho" [idade],"anos, moro em" [cidade], "e sou " [profissão])
+}
+console.log (frase) exercicio1(
+)
+//------------------------------------------------------------------------------------------//
+
