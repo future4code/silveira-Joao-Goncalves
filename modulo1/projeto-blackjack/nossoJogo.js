932af410d4f2336330141d91200f002189d04d5d
Joãopegnolate <joaopegnolate@gmail.com>
2022-02-27 20:05:11 -0300

    projeto-black-jack

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -10,3 +10,41 @@
  * 
  * 
  */
+
+
+    const blackjack = texto (prompt("Boas vindas ao jogo de blackjack")),
+
+if(confirm('quer iniciar uma nova rodada?')) {
+   "iniciar uma nova rodada" (ok) 
+}else { 
+"o jogo acabou" (cancel)
+}
+function comprarCarta(){
+   const cartas = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+   
+   const naipes = ["♦️", "♥️", "♣️", "♠️"]
+
+   const naipe = naipes[Math.floor(Math.random() * 4)]
+
+   let valor
+ 
+   if (numero === "A") {
+      valor = 11
+   } else if (numero === "J" || numero === "Q" || numero === "K") {
+     valor = 10
+   } else { 
+     valor = Number(numero)
+   }
+   const carta = {
+      texto: numero + naipe,
+      valor: valor
+    }
+  
+    return carta
+   }
+
+console.log(carta.texto)
+
+
+
